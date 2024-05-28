c520ec20aba696acffa4323f2bd645972af828f1
Javier Martín <alumno20civica@gmail.com>
2024-05-28 14:51:46 +0000

    New commit

--- models/staging/SQL_SERVER_DBO/stg_sql_server__orders.sql
+++ models/staging/SQL_SERVER_DBO/stg_sql_server__orders.sql
@@ -4,6 +4,12 @@ WITH src_orders AS (
     FROM {{ source('sql_server_dbo', 'orders') }}
     ),
 
+
+promos AS (    
+    SELECT promo_id
+    FROM {{ source('sql_server_dbo', 'promos') }}
+),
+
 renamed_casted AS (
     SELECT
           order_id
@@ -11,7 +17,8 @@ renamed_casted AS (
         , shipping_cost
         , address_id
         , created_at
-        , promo_id
+        , CASE WHEN b.promo_id is null then md5('Sin promocion')
+            ELSE md5(b.promo_id) END AS promo_id
         , estimated_delivery_at
         , order_cost
         , user_id
@@ -21,7 +28,9 @@ renamed_casted AS (
         , status
         , _fivetran_deleted AS date_deleted
         , _fivetran_synced AS date_load
-    FROM src_orders
+    FROM src_orders a
+    LEFT JOIN promos b
+    ON a.promo_id = b.promo_id
     )
 
 SELECT * FROM renamed_casted
--- models/staging/SQL_SERVER_DBO/stg_sql_server__promos.sql
+++ models/staging/SQL_SERVER_DBO/stg_sql_server__promos.sql
@@ -1,16 +1,29 @@
 WITH src_promos AS (
-    SELECT * 
+    SELECT *
     FROM {{ source('sql_server_dbo', 'promos') }}
     ),
 
 renamed_casted AS (
     SELECT
-          promo_id
-        , discount
+          md5(promo_id) as promo_id
+        , promo_id AS promo_name
+        , discount AS discount_dollars
         , status
         , _fivetran_deleted AS date_deleted
-        , _fivetran_synced AS date_load
+        , _fivetran_synced AS date_load_UTC
     FROM src_promos
-    )
+    ),
+
+new_row as (
+    select
+        md5('Sin promocion') as promo_id,
+        'Sin promocion' as promo_name,
+        0 as discount_dollars,  
+        'active' as satus,  
+        null as date_deleted, 
+        null as date_load_UTC  
+)
 
 SELECT * FROM renamed_casted
+UNION
+SELECT * FROM new_row
