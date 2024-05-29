ce33cb64ad0de98a60785b98cc68e0af1b0555f6
Javier Martín <alumno20civica@gmail.com>
2024-05-29 14:41:53 +0000

    new

--- models/staging/SQL_SERVER_DBO/stg_sql_server__users.sql
+++ models/staging/SQL_SERVER_DBO/stg_sql_server__users.sql
@@ -3,20 +3,30 @@ WITH src_users AS (
     FROM {{ source('sql_server_dbo', 'users') }}
     ),
 
+user_orders AS (
+    SELECT  
+        user_id,
+        count(*) as total_orders
+    FROM {{ source('sql_server_dbo', 'orders') }}
+    GROUP BY user_id
+),
+
 renamed_casted AS (
     SELECT
-          user_id
-        , updated_at
-        , address_id
-        , last_name
-        , created_at
-        , phone_number
-        , total_orders
-        , first_name
-        , email
-        , coalesce(_fivetran_deleted, false) AS date_deleted
-        , convert_timezone('UTC',_fivetran_synced) AS date_load
-    FROM src_users
+          a.user_id,
+          first_name,
+          last_name,
+          phone_number,
+          email,
+          b.total_orders,
+          address_id,
+          convert_timezone('UTC',updated_at) as updated_at_utc,
+          convert_timezone('UTC',created_at) as created_at_utc,
+          coalesce(_fivetran_deleted, false) AS date_deleted,
+          convert_timezone('UTC',_fivetran_synced) AS date_load
+    FROM src_users a
+    LEFT JOIN user_orders b
+    ON a.user_id = b.user_id
     )
 
 SELECT * FROM renamed_casted
